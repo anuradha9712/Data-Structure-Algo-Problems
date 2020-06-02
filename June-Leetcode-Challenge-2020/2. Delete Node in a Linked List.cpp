fae7a7cf0d1c55ec8e5f4cc12cee9b6893ac552a
anuradha9712 <46045493+anuradha9712@users.noreply.github.com>
2020-06-03 02:28:39 +0530

    Day 2

--- June-Leetcode-Challenge-2020/2. Delete Node in a Linked List.cpp
+++ June-Leetcode-Challenge-2020/2. Delete Node in a Linked List.cpp
@@ -1,5 +1,6 @@
 # 2. Delete Node in a Linked List
 
+'''
 Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
 
 Given linked list -- head = [4,5,1,9], which looks like following:
@@ -20,6 +21,8 @@ All of the nodes' values will be unique.
 The given node will not be the tail and it will always be a valid node of the linked list.
 Do not return anything from your function.
 
+'''
+    
 /**
  * Definition for singly-linked list.
  * struct ListNode {
