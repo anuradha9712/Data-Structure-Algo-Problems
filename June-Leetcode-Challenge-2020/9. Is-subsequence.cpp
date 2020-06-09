cf0b66f365a8c6cc7e3413d6a0593e7fb0ac7d55
anuradha9712 <46045493+anuradha9712@users.noreply.github.com>
2020-06-09 14:13:11 +0530

    Day 9

---
+++ June-Leetcode-Challenge-2020/9. Is-subsequence.cpp
@@ -0,0 +1,63 @@
+# is subsequence
+
+Given a string s and a string t, check if s is subsequence of t.
+
+A subsequence of a string is a new string which is formed from the original string
+by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters.
+(ie, "ace" is a subsequence of "abcde" while "aec" is not).
+
+Follow up:
+If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B,
+and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?
+
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+Approach 1: time complexity: O(m)
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+        int n = s.size();
+        int m = t.size();
+        
+        int j = 0;
+        for(int i=0;i<m;i++){
+            if(s[j]==t[i]) j++;
+        }
+        
+        return (j==n)? true: false;
+    }
+};
+
+
+/*========================================================*/
+// Approach 2: (using DP) time complexity:- O(mn)
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+        int n = s.size();
+        int m = t.size();
+        int dp[n+1][m+1];
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                if(i==0 || j==0) dp[i][j] = 0;
+                
+                else if(s[i-1]==t[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+            }
+        }
+        
+        if(dp[n][m]== n) return true;
+        else return false;
+        
+    }
+};
